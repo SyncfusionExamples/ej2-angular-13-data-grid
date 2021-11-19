a08bad6992b98370629814b3fc93a283c30c9c57
Sathish Kumar Rajendran <sathishkumar.rajendran@syncfusion.com>
2021-11-19 12:07:54 +0530

    Updated grid with angular 13 sample

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+
+import { Component, OnInit } from '@angular/core';
+import { data } from './datasource';
+
+@Component({
+    selector: 'app-root',
+    template: `<ejs-grid [dataSource]='data'> </ejs-grid>`
+})
+export class AppComponent implements OnInit {
+
+    public data!: object[];
+    title = 'angular'
+
+    ngOnInit(): void {
+        this.data = data;
+    }
+}
+
+
